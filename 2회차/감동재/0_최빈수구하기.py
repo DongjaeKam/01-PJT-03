8cd621a1bfc1dd8b8d0a0730f77c1045395b27d7
DongjaeKam <dongjaekam1994@gmail.com>
2022-07-29 17:52:09 +0900

    0729 감동재

--- 2회차/감동재/0_최빈수구하기.py
+++ 2회차/감동재/0_최빈수구하기.py
@@ -1,3 +1,27 @@
 import sys
 
 sys.stdin = open("_최빈수구하기.txt")
+
+T = int(input())
+ 
+for test_case in range(0,T):
+    x = input()
+    I =input().split()
+    dic ={}
+ 
+    for i in I:
+        if i in dic:
+            dic[i]+=1
+        else :
+            dic[i] = 1
+ 
+    max_cnt = 0
+    current_max =0
+ 
+    for d in dic:
+        if dic[d] >= max_cnt:
+            max_cnt = dic[d]
+            if int(d) >= current_max:
+                current_max = int(d)
+ 
+    print(f"#{test_case+1} {current_max}")
